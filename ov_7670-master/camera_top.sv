dc18a4bb892ae92cf97eec08afc16e0b5031f4d8
ravil99 <48554221+ravil99@users.noreply.github.com>
2020-01-26 21:41:09 +0600

    Create camera_top.sv
    Начало работы с камерой

---
+++ ov_7670-master/camera_top.sv
@@ -0,0 +1,43 @@
+module camera_top 
+        (
+           input            ipclk,
+           input            ivsync,
+           input            ihref,
+           input            idata,
+           input            iclk,
+           input            iclk_sccb,
+           input            ireset,
+           input            isio_d,
+           input            istart,
+           output           owr_en,
+           output           oaddr,
+           output           odata_out,
+           output           opwdn,
+           output           osio_c,
+           output           odone
+        );
+
+    ov_7670_capture 
+        (
+           .pclk            (ipclk),
+           .vsync           (ivsync),
+           .href            (ihref),
+           .data            (idata),
+           .addr            (oaddr),
+           .data_out        (odata_out),
+           .write_en        (owr_en),
+        );
+
+    ov_7670_init
+        (
+            .clk            (iclk),
+            .clk_sccb       (iclk_sccb),
+            .reset          (ireset),
+            .pwdn           (opwdn),
+            .sio_d          (isio_d),
+            .sio_c          (osio_c),
+            .start          (istart),
+            .done           (odone)
+        );
+
+endmodule

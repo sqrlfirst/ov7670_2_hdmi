57a06a1db7bcb33c784571316d9410584e8c4331
Egor <egor.dergunov1996@gmail.com>
2020-01-26 21:37:58 +0700

    add pll begin work with camera connection

--- top_module.sv
+++ top_module.sv
@@ -16,24 +16,45 @@ module top_module(
     inout                       iosi5338_i2c_sda,   // PIN g11
 
     // HDMI PHY interface ADV7513
-    output [23:0]               ohdmi_tx_d   ;      // PIN  v23  aa26 w25  w26  v24  v25  u24  t23  t24  t26  r23   r25
+    output [23:0]               ohdmi_tx_d   ,      // PIN  v23  aa26 w25  w26  v24  v25  u24  t23  t24  t26  r23   r25
                                                     // line 0    1    2    3    4    5    6    7    8    9    10    11                
                                                     // PIN  p22  p23  n25  p26  p21  r24  r26  ab26 aa24 ab25 ac25  ad25
                                                     // line 12   13   14   15   16   17   18   19   20   21   22    23  
-    output                      ohdmi_tx_clk ;      // PIN  y25
-    output                      ohdmi_tx_de  ;      // PIN  y26
-    output                      ohdmi_tx_hs  ;      // PIN  u26
-    output                      ohdmi_tx_vs  ;      // PIN  u25
-    input                       ohdmi_tx_int ;      // PIN  t12
-    inout                       ohdmi_i2c_sda;      // PIN  b7  
-    output                      ohdmi_i2c_scl;      // PIN  g11
+    output                      ohdmi_tx_clk ,      // PIN  y25
+    output                      ohdmi_tx_de  ,      // PIN  y26
+    output                      ohdmi_tx_hs  ,      // PIN  u26
+    output                      ohdmi_tx_vs  ,      // PIN  u25
+    input                       ohdmi_tx_int ,      // PIN  t12
+    inout                       ohdmi_i2c_sda,      // PIN  b7  
+    output                      ohdmi_i2c_scl,      // PIN  g11
     // Micro SD
-    output                      omicro_sd_clk;      // PIN  ab6 
-    inout                       omicro_sd_cmd;      // PIN  w8 
-    inout  [3:0]                omicro_sd_dat;      // PIN  u7  t7  v8  t8         
+    output                      omicro_sd_clk,      // PIN  ab6 
+    inout                       omicro_sd_cmd,      // PIN  w8 
+    inout  [3:0]                omicro_sd_dat,      // PIN  u7  t7  v8  t8         
                                                     // line 0   1   2   3
 
+    // camera interface OV7670
+    input [7:0]                 icam_data,          // GPIO pins 18..25
+                                                    // pin   
+                                                    // 
+
+
 );
+
+    // pll 
+    // c0   24   Mhz 
+    // c1   100  Mhz
+    // c2   150  Mhz
+    pll_24Mhz (
+		.refclk             ( ),   //  refclk.clk
+		.rst                ( ),      //   reset.reset
+		.outclk_0           ( ), // outclk0.clk
+		.outclk_1           ( ), // outclk1.clk
+		.outclk_2           ( ), // outclk2.clk
+		.locked             ( ) //  locked.export
+	);
+
+
 
 
 

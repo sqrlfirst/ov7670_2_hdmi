e95d2930a0de29d4c63ac88989be58e294ea28f3
Egor <egor.dergunov1996@gmail.com>
2020-02-09 22:33:23 +0700

    veri not useful

---
+++ image_work/compression/im_compression_tb.sv
@@ -0,0 +1,16 @@
+module im_compression_tb;
+
+    localparam in_width = ;
+
+    logic [639:0][479:0][2:0][7:0]        pic_batman;
+
+
+
+    initial begin
+        pic_batman = $readmem(batman.mem)
+    end
+
+
+
+
+endmodule 
